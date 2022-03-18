9ca30f9c5fc7e3942fe2fe67115a61c4c190d3ab
Faav <faavemc@gmail.com>
2022-03-18 18:31:21 -0400

    Update mfobj-parser.js

--- mfobj-parser.js
+++ mfobj-parser.js
@@ -21,7 +21,7 @@ mfobj.parse  = function(text, _settings = {}) {
     if (_settings !== {}) { 
       if (_settings.color) settings.color = _settings.color;
       if (_settings.width) settings.width = _settings.width;
-      if (_settings.height) settings.color = _settings.height;
+      if (_settings.height) settings.height = _settings.height;
     }
     if (settings.color == "black") return text.split('￼').join(`<img src="https://cdn.jsdelivr.net/gh/bribes/mobile-obj-parser/images/obj_black.png" width="${settings.width}" height="${settings.height}" class="mobile-obj obj-black">`);
     return text.split('￼').join(`<img src="https://cdn.jsdelivr.net/gh/bribes/mobile-obj-parser/images/obj_white.png" width="${settings.width}" height="${settings.height}" class="mobile-obj obj-white">`);
