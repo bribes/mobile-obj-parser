700b5e101b0f39d3e8535a07df6c8f60fb32995e
Faav <faavemc@gmail.com>
2022-03-18 16:48:35 -0400

    Update mfobj-parser.js

--- mfobj-parser.js
+++ mfobj-parser.js
@@ -1,1 +1,27 @@
+function isMobile() {
+  try{ document.createEvent("TouchEvent"); return true; }
+  catch(e){ return false; }
+}
 
+var mfobj = {};
+
+mfobj.settings = {
+  color: "white",
+  width: 16,
+  height: 12
+}; // color: white | black
+
+mfobj.parse  = function(text, _settings = {}) { 
+  if (isMobile() == true) {
+    var settings = mfobj.settings;
+    if (_settings !== {}) { 
+      if (_settings.color) settings.color = _settings.color;
+      if (_settings.width) settings.width = _settings.width;
+      if (_settings.height) settings.color = _settings.height;
+    }
+    if (settings.color == "black") return text.split('￼').join(`<img src="images/obj_black.png" width="${settings.width}" height="${mfobj.settings.height}" class="mobile-obj obj-black">`);
+    return text.split('￼').join(`<img src="images/obj_white.png" width="${settings.width}" height="${settings.height}" class="mobile-obj obj-white">`);
+  } else {
+    return text;
+  }
+}
